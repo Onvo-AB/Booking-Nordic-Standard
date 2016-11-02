25443a08ec4902f4d2105812c21c3b429cff63ac
Robin Andersson <robin@rows.se>
2016-11-02 13:40:05 +0100

    Changed clientKey[16] to string

--- draft1/Examples/ANSI C/clientToken/main.c
+++ draft1/Examples/ANSI C/clientToken/main.c
@@ -6,43 +6,9 @@
 #include <time.h>
 
 
-
-static inline int parseUUID(const char* _Str, uint8_t _Data[16])
-{
-	int length = strlen(_Str);
-
-	const char* ptr = _Str;
-	unsigned int currentValue;
-	unsigned int i = 0;
-	
-	while(*(ptr) != 0 && *(ptr + 1) != 0 && (length <= 0 || (ptr - _Str) < length) && i < 16)
-	{
-		if(*(ptr) == '-')
-		{
-			ptr++;
-		}
-		else
-		{
-			if(sscanf((const char*)ptr, "%02x", &currentValue) != 1)
-			{
-				return -1;
-			}
-			else
-			{
-				_Data[i] = (uint8_t)currentValue;
-				ptr += 2;
-				i++;
-			}
-		}
-	}
-	
-	
-	return i;
-}
-
 int main()
 {
-	uint64_t systemTime = 1475226019lu; //time(NULL);
+	uint64_t systemTime = 1475226019lu;
 	const char* clientID = "9818d49a-005d-4a83-93b3-9de04a6a5225";
 	const char* method = "GetCustomerData";
 
@@ -51,13 +17,9 @@ int main()
 	char message[512];
 	sprintf(message, "%lu%s%s", systemTime, clientID, method);
 
-	uint8_t key[16];
-	parseUUID(clientKey, key);
-
-
 	uint8_t hash[SHA1_DIGEST_LENGTH];
 	memset(hash, 0, sizeof(hash));
-	hmac_sha1(key, sizeof(key), (const uint8_t*)message, strlen(message), hash, sizeof(hash));
+	hmac_sha1((unsigned char*)clientKey, strlen(clientKey), (const uint8_t*)message, strlen(message), hash, sizeof(hash));
 
 	char clientToken[64];
 	char* ptr = clientToken;
